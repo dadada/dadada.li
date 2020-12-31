f35f8e137ce6b685821049cabe00fa9d26a54282
dadada <dadada@dadada.li>
2020-12-31 12:00:54 +0100

    Fixup pandoc long option

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find . -iname "*.md" -type f -exec sh -c 'pandoc -s -f commonmark -t html --template src/template.html -o "''${0%.md}.html" "''${0}"' {} \;
+find . -iname "*.md" -type f -exec sh -c 'pandoc -s -f commonmark -t html --template=src/template.html -o "''${0%.md}.html" "''${0}"' {} \;
